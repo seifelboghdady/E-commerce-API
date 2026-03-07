d9a31e237fc0e5df5ff2d4b477e2c1458e639afa
unknown <seifelboghdady@std.mans.edu.eg>
2026-03-07 23:04:03 +0200

    test getandset in sequelize

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import { raw } from 'express';
-import { Sequelize, DataTypes, Model, json, Op } from 'sequelize';
+import { Sequelize, DataTypes, Model, json, Op, where } from 'sequelize';
 
 
 //Passing parameters separately (sqlite)
@@ -16,7 +16,12 @@ BOOK.init(
         },
         author:{
             type: DataTypes.STRING,
-            defaultValue: "Unknown Author"
+            defaultValue: "Unknown Author",
+            //we want return every member in uppercase
+            get(){
+                const rowdata = this.getDataValue('auther');
+                return rowdata? rowdata.toUpperCase(): null;
+            }
         },
         pages:{
             type: DataTypes.INTEGER
@@ -39,67 +44,192 @@ BOOK.init(
     }
     
     
-    
-// }    
+const book1 = await BOOK.create({title: 'Node.js Guide', author: 'John Doe', pages :200});
+const book2= await BOOK.create({author: "Jane Smith" ,title : 'Sequelize Master', pages :150});
+const book3= await BOOK.create({title : 'Database Design', pages :300});
 
-// // StartApp();
-const book1= await BOOK.create({title: "programming lang",pages:100, isAvailable: true})
-// // console.log(book1.title);
+console.log(book1.author);
+console.log(book2.author);
+console.log(book3.author);
+
+
+// // }    
+// // // StartApp();
+// // const book1= await BOOK.create({title: "programming lang",pages:100, isAvailable: true})
+// // // console.log(book1.title);
+// // // console.log(JSON.stringify(book1));
+// // //update ada manuall
+
+// // book1.pages= 200;
+// // book1.author = "seif ashraf";
+// // book1.save();
+// // //-------------------------------------------------
 // // console.log(JSON.stringify(book1));
-// //update ada manuall
+// /*
+// Your Task:
+// Write a Node.js script that performs the following sequential operations inside an async function:
 
-// book1.pages= 200;
-// book1.author = "seif ashraf";
-// book1.save();
-// //-------------------------------------------------
-// console.log(JSON.stringify(book1));
+// Bulk Creation (Insert):
+
+// Add at least 3 books to the database in one go (or using multiple create calls).
+
+// Data example: * Book 1: Title "Node.js Guide", Author "John Doe", Pages 200.
+
+// Book 2: Title "Sequelize Master", Author "Jane Smith", Pages 150.
+
+// Book 3: Title "Database Design", Pages 300 (keep Author as default).
+
+// Smart Searching (Finders):
+
+// Find one book by its ID (Primary Key) and print its title.
+
+// Find the first book written by "Jane Smith" using findOne.
+
+// Search for all books that have more than 180 pages.
+
+// Updating Data:
+
+// Find the book "Node.js Guide" and change its isAvailable status to false.
+
+// Use the increment method to add 10 pages to the book "Sequelize Master".
+
+// Pagination & Counting:
+
+// Use findAndCountAll to get only the first 2 books (limit: 2) and print the total count of books in the database.
+
+// Cleanup (Delete):
+
+// Delete the book with ID: 3 from the database.
+
+// */
+// const book1 = await BOOK.create({title: 'Node.js Guide', author: 'John Doe', pages :200});
+// const book2= await BOOK.create({author: "Jane Smith" ,title : 'Sequelize Master', pages :150});
+// const book3= await BOOK.create({title : 'Database Design', pages :300});
 
 
-const book2= await BOOK.create({author: "seif elboghdady" ,title : 'cpp', pages :180, isAvailable: false});
-const book3= await BOOK.create({author: "Samir" ,title : 'cpp', pages :500, isAvailable: true});
-// console.log(JSON.stringify(book2));
-// book2.set({
-//     title : 'cpp with seif',
-//     pages : 181,
-//     author: 'samir',
-//     isAvailable: true
-// })
 
-// console.log(JSON.stringify(book2));
-// book2.increment('pages');
-// book2.save();
-// console.log(JSON.stringify(book2));
+// //Find one book by its ID (Primary Key) and print its title.
+// const Books1 = await BOOK.findByPk(1,{
+//     attributes: ['title']
+// });
+// console.log(JSON.stringify(Books1));
+
+// //Find the first book written by "Jane Smith" using findOne.
+// const bookq1 = await BOOK.findOne({
+//     where:{
+//         author:{
+
+//             [Op.like] : 'Jane Smith'
+//         }
+//     }
+// });
+// console.log(JSON.stringify(bookq1));
 
 
-// const users = await BOOK.findAll(
+// //Search for all books that have more than 180 pages.
+// const books2= await  BOOK.findAll({
+//     where:{
+//         pages:{
+//             [Op.gt]: 180
+//         }
+//     }
+// });
+// console.log(JSON.stringify(books2));
+// console.log();
+// console.log();
+
+
+// //Find the book "Node.js Guide" and change its isAvailable status to false.
+// const updatedelement= await BOOK.update({isAvailable: false},
 //     {
-//         attributes :["author", [sequelize.fn('COUNT', sequelize.col('pages')),'TotalBooks']],
-//         where : {
-//             pages : {
-//                 [Op.gt]:100
-//             },
-//             isAvailable:{
-//                 [Op.eq]:true
+//         where:{
+//             title:{
+//                 [Op.like]: "Node.js Guide"
 //             }
-
 //         }
 //     }
 // );
 
-// const users2 = await BOOK.findAll({
-//     attributes:[
-//         'pages',
-//         'author',
-//         'isAvailable'
-//     ],
-//     order: [
-//         ['pages', 'DESC']
-//     ]
-// });
+// // console.log(JSON.stringify(await BOOK.findAll({where: {title:"Node.js Guide"}})));
+// console.log(JSON.stringify(updatedelement));
+
+// console.log();
 
 
-// // console.log(JSON.stringify(users));
-// console.log(JSON.stringify(users2));
+
+
+// //Use the increment method to add 10 pages to the book "Sequelize Master".
+// const incrementedelement = await BOOK.increment({pages : 10}, {where: {title : {[Op.like]: "Sequelize Master"}}});
+
+// console.log(`this is incremented` ,JSON.stringify(incrementedelement));
+// console.log();
+
+
+
+// // console.log(JSON.stringify(await BOOK.findAll({where: {title:"Sequelize Master"}})));
+
+// //Use findAndCountAll to get only the first 2 books (limit: 2) and print the total count of books in the database.
+// const {count, rows}= await BOOK.findAndCountAll({limit:2});
+// console.log(count);
+// console.log();
+
+
+
+// //Delete the book with ID: 3 from the database.
+// const deletebook = await BOOK.destroy({
+//     where:{
+//         id :3
+//     }
+// })
+// console.log(deletebook);
+
+
+
+
+
+// // console.log(JSON.stringify(book2));
+// // book2.set({
+// //     title : 'cpp with seif',
+// //     pages : 181,
+// //     author: 'samir',
+// //     isAvailable: true
+// // })
+
+// // console.log(JSON.stringify(book2));
+// // book2.increment('pages');
+// // book2.save();
+// // console.log(JSON.stringify(book2));
+
+
+// // const users = await BOOK.findAll(
+// //     {
+// //         attributes :["author", [sequelize.fn('COUNT', sequelize.col('pages')),'TotalBooks']],
+// //         where : {
+// //             pages : {
+// //                 [Op.gt]:100
+// //             },
+// //             isAvailable:{
+// //                 [Op.eq]:true
+// //             }
+
+// //         }
+// //     }
+// // );
+
+// // const users2 = await BOOK.findAll({
+// //     attributes:[
+// //         'pages',
+// //         'author',
+// //         'isAvailable'
+// //     ],
+// //     order: [
+// //         ['pages', 'DESC']
+// //     ]
+// // });
+
+
+// // // console.log(JSON.stringify(users));
+// // console.log(JSON.stringify(users2));
 
 
 
