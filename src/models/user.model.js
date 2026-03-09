1f494bd63c933adad26be067d1a0f9a5c072b281
unknown <seifelboghdady@std.mans.edu.eg>
2026-03-10 01:42:08 +0200

    edit to begin in project

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,11 @@
 /*
+Description
+Task: Define user schema
+Input
+User registration data
+Output
+User stored in database
+Fields:
 User
 - id
 - name
@@ -8,51 +15,51 @@ User
 - createdAt
 */
 
-// const { Sequelize, DataTypes } = require('sequelize');
 
-// //Passing parameters separately (sqlite)
-// //Disable SQL logging in the console to keep the output clean.
-// const sequelize = new Sequelize('sqlite::memory:',{logging: false});
 
-// sequelize.define(
-//     'BOOK',
-//     {
-//         title: {
-//             type: DataTypes.STRING,
-//             allowNull : false
-//         },
-//         author:{
-//             type: DataTypes.STRING,
-//             defaultValue: "Unknown Author"
-//         },
-//         pages:{
-//             type: DataTypes.INTEGER
-//         },
-//         isAvailable:{
-//             type: DataTypes.BOOLEAN,
-//             defaultValue: true
-//         }
-//     },{sequelize, timestamps: false}
-// );
+import { hash } from "bcrypt";
+import { DataTypes, Model, Sequelize } from "sequelize";
 
-// async function StartApp(){
+const sequelize = new Sequelize({
+    dialect:'sqlite',
+    storage: 'DataBase.sqlite',
+    logging: false,
+});
+
+class User extends Model{}
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name:{
+        type: DataTypes.STRING,
+        default: 'Unkown User',
+        validate:{
+            min: 4
+        }
+    },
+    email:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate:{
+            isEmail: true
+        }
+    },
+    password:{
+        type: DataTypes.STRING,
+        set(value){
+            this.setDataValue('password', hash(value));
+        }
+    },
+    role:{
+        type: DataTypes.ENUM("user", "admin"),
+        defaultValue: 'user'
+    }
     
-//     try {
-//         await sequelize.authenticate();
-//         console.log('Connection has been established successfully.');
-//     } catch (error) {
-//         console.error('Unable to connect to the database:', error);
-//     }
-    
-//     await User.sync();
-// }
+},{sequelize, createdAt: true});
 
-// StartApp();
-// const book1= await BOOK.create({title: "programming lang",page:100, isAvailable: true})
-// console.log(book1.name);
-
-
-
-
-
+module.exports = User;
 
