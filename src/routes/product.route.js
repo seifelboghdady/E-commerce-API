e42b7992fa0a463706e8bdad36b9dc8fa8c0c395
unknown <seifelboghdady@std.mans.edu.eg>
2026-03-26 21:34:06 +0200

    add route of delete product func

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,6 +1,6 @@
 import {auth} from '../middleware/authToken.js'
 import { Router } from 'express';
-import { addProduct, getAllProduct, getProductByID, updateProduct } from '../controllers/product.controller.js';
+import { addProduct, deleteProduct, getAllProduct, getProductByID, updateProduct } from '../controllers/product.controller.js';
 const routerproduct = Router();
 
 routerproduct
@@ -8,4 +8,5 @@ routerproduct
     .get('/api/products',auth,getAllProduct)
     .get('/api/products/:id',auth,getProductByID)
     .put('/api/products/:id', auth, updateProduct)
+    .delete('/api/products/:id', auth, deleteProduct)
 export {routerproduct};
