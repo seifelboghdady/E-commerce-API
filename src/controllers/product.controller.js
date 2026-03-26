4557c2a650a9c23092095be8dde0fc0f9e982c6c
unknown <seifelboghdady@std.mans.edu.eg>
2026-03-26 21:32:40 +0200

    add deletedproduct func

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -126,3 +126,28 @@ export const updateProduct = async(req, res)=>{
         });        
     }
 }
+
+export const deleteProduct = async (req, res)=>{
+    try {
+        const {id} = req.params;
+        const deletedProduct = await Product.destroy({where:{id}});
+        if (deletedProduct === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "Product not found or already deleted"
+            });
+        }
+        return res.status(200).json({
+            success: true,
+            message: "Product deleted successfully",
+            data: deletedProduct
+        });
+
+    } catch (error) {
+        console.error("Error fetching product by ID:", error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });   
+    }
+}
