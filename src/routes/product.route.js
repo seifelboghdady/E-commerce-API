5e5816e0aaacb47c1592fada4d99a6d7fa6ca66f
unknown <seifelboghdady@std.mans.edu.eg>
2026-03-26 20:58:12 +0200

    add routes updateproduct

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,12 +1,11 @@
 import {auth} from '../middleware/authToken.js'
 import { Router } from 'express';
-import { addProduct, getAllProduct, getProductByID } from '../controllers/product.controller.js';
+import { addProduct, getAllProduct, getProductByID, updateProduct } from '../controllers/product.controller.js';
 const routerproduct = Router();
 
 routerproduct
     .post('/api/products',auth,addProduct)
     .get('/api/products',auth,getAllProduct)
     .get('/api/products/:id',auth,getProductByID)
-    
-
+    .put('/api/products/:id', auth, updateProduct)
 export {routerproduct};
