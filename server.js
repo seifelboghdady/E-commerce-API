987db2e69a3d92003887908c1a925dbd26297303
unknown <seifelboghdady@std.mans.edu.eg>
2026-04-13 07:43:34 +0200

    update DBs and server js

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ import sequelize from './src/config/sequelize.js';
 import {Order, Product, User, OrderProduct} from './src/models/Association.model.js';
 import {router} from './src/routes/user.route.js';
 import { routerproduct } from './src/routes/product.route.js';
+import { cartRouter } from './src/routes/cart.route.js';
 const app = express();
 app.use(express.json());
 app.use(routerproduct);
 app.use(router);
+app.use(cartRouter);
 
-await sequelize.sync({ alter: true });
+// await sequelize.sync({ alter: true });
+await sequelize.sync();
 
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000');
